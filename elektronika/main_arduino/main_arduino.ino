49f4f34536ee2f739ce677d2f1b458bd07b252c9
Kerowsky <kacpidos2013@gmail.com>
2025-10-07 21:03:55 +0200

    dodanie main_arduino

--- elektronika/main_arduino/main_arduino.ino
+++ elektronika/main_arduino/main_arduino.ino
@@ -1,9 +1,49 @@
+#include "Arduino_FreeRTOS.h"
+#include "DHT.h"
+
+///Pinout
+
+#define DHTPIN 11
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void taskBlink(void *param){
+  pinMode(13, OUTPUT);
+  while(1){
+    digitalWrite(13,HIGH);
+    vTaskDelay(1000 / portTICK_PERIOD_MS );
+    digitalWrite(13, LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+void DHT_ReadData(void *param){
+  while(1){
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Błąd odczytu z DHT!");
+    return;
+    }
+    Serial.print("Wilgotnosc: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+
+    Serial.print("Temperatura: ");
+    Serial.print(humidity);
+    Serial.print(" *C\t");
+    vTaskDelay(2000 / portTICK_PERIOD_MS );
+  }
+
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  xTaskCreate(taskBlink, "LED", 128, NULL, 1, NULL);
+  xTaskCreate(DHT_ReadData, "OdczytDHT", 128, NULL, 1, NULL);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
