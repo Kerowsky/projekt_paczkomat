c2da9a43bb4bc127f5f4f1da447c6a7b31ed1908
RadekChmielewski1 <164099139+RadekChmielewski1@users.noreply.github.com>
2025-10-08 13:16:17 +0200

    Update skrypt_aktualizacja_8_10_2025.sql

---
+++ strona/server/skrypt.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE InteligentnaSkrytka;
+
+USE InteligentnaSkrytka;
+
+
+CREATE TABLE IF NOT EXISTS Paczki(
+    id_paczki INT PRIMARY KEY AUTO_INCREMENT,
+    id_klienta INT,
+    id_kuriera INT,
+    id_skrytki INT,
+    status ENUM('NADANA','W_PACZKOMACIE','ODEBRANA'),
+);
+
+CREATE TABLE IF NOT EXISTS Paczkomat
+(
+    id_paczkomatu INT PRIMARY KEY AUTO_INCREMENT,
+    id_skrytki    INT,
+    rozmiar       ENUM ('S','M','L'),
+    status        ENUM ('WOLNA','ZAJĘTA') DEFAULT 'WOLNA',
+    
+);
+
+CREATE TABLE IF NOT EXISTS Uzytkownicy(
+    id_klienta INT,
+    id_kuriera INT,
+    rola ENUM('ADMIN','KURIER','KLIENT')
+);
+
