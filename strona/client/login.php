6f82e90ef11c53a0f53a6b88d05d6f05f2dafae9
Kerowsky <kacpidos2013@gmail.com>
2025-10-11 20:12:48 +0200

    Edycja skryptu sql

--- strona/client/login.php
+++ strona/client/login.php
@@ -5,17 +5,13 @@ $conn = @new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "select * from paczki";
 
 $login = isset($_POST['username']) ? $_POST['username'] : '';
 $haslo = isset($_POST['password']) ? $_POST['password'] : '';
 
+$sql = "select * from Uzytkownicy where login='$login' and haslo='$haslo' ";
 
-// przykładowe dane (docelowo będą w bazie)
-$poprawny_login = "admin";
-$poprawne_haslo = "1234";
-
-if ($login === $poprawny_login && $haslo === $poprawne_haslo) {
+if ($result = $conn->query($sql)) {
     $_SESSION['zalogowany'] = true;
     echo "✅ Zalogowano poprawnie!";
     echo "<br><a href='panel.php'>Przejdź do panelu</a>";
@@ -25,6 +21,7 @@ if ($login === $poprawny_login && $haslo === $poprawne_haslo) {
 }
 ?>
 <?php
+$sql = "select * from paczki";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
--- strona/server/skrypt.sql
+++ strona/server/skrypt.sql
@@ -2,30 +2,53 @@ CREATE DATABASE InteligentnaSkrytka;
 
 USE InteligentnaSkrytka;
 
+CREATE TABLE IF NOT EXISTS Paczkomat (
+    id_skrytki INT PRIMARY KEY NOT NULL,
+    rozmiar ENUM('S','M','L') NOT NULL,
+    status ENUM('WOLNA','ZAJETA') DEFAULT 'WOLNA',
+    CHECK (id_skrytki IN (1, 2, 3))
+);
+
+
+CREATE TABLE IF NOT EXISTS Uzytkownicy(
+    id_uzytkownika INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
+    login VARCHAR(255) NOT NULL,
+    haslo VARCHAR(255) NOT NULL,
+    imie VARCHAR(255) NOT NULL,
+    nazwisko VARCHAR(255) NOT NULL,
+    rola ENUM('ADMIN','KURIER','KLIENT') DEFAULT 'KLIENT'
+);
 
 CREATE TABLE IF NOT EXISTS Paczki(
-    id_paczki INT PRIMARY KEY AUTO_INCREMENT,
-    id_uzytkownika INT NOT NULL ,
-    id_skrytki INT NOT NULL,
+    id_paczki INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+    id_uzytkownika INT NOT NULL,
+    id_skrytki INT,
     status ENUM('NADANA','W_PACZKOMACIE','ODEBRANA') NOT NULL,
+    nadawca VARCHAR(255),
     FOREIGN KEY (id_uzytkownika) REFERENCES Uzytkownicy(id_uzytkownika),
     FOREIGN KEY (id_skrytki) REFERENCES Paczkomat(id_skrytki)
 
 );
 
-CREATE TABLE IF NOT EXISTS Paczkomat
-(
-    id_skrytki    INT NOT NULL,
-    rozmiar       ENUM ('S','M','L') NOT NULL,
-    status        ENUM ('WOLNA','ZAJĘTA') DEFAULT 'WOLNA'
-);
+##              Przykładowi użytkownicy     ##
+INSERT INTO Uzytkownicy (login, haslo, imie, nazwisko, rola)
+VALUES
+    ('admin', '1234', 'Jan', 'Nowak', 'ADMIN'),
+    ('kurier_jan', '1234', 'Jan', 'Kowalski', 'KURIER'),
+    ('kurier_anna', '1234', 'Anna', 'Wiśniewska', 'KURIER'),
+    ('klient_marek', '1234', 'Marek', 'Zieliński', 'KLIENT'),
+    ('klient_ewa', '1234', 'Ewa', 'Kamińska', 'KLIENT'),
+    ('klient_piotr', '1234', 'Piotr', 'Lewandowski', 'KLIENT');
 
-CREATE TABLE IF NOT EXISTS Uzytkownicy(
-    id_uzytkownika INT  PRIMARY KEY AUTO_INCREMENT NOT NULL ,
-    login VARCHAR(255) NOT NULL,
-    haslo VARCHAR(255) NOT NULL,
-    rola ENUM('ADMIN','KURIER','KLIENT') DEFAULT 'KLIENT'
-);
+INSERT INTO Paczkomat (id_skrytki, rozmiar, status)
+VALUES
+    (1, 'L', 'WOLNA'),
+    (2, 'M', 'WOLNA'),
+    (3, 'M', 'ZAJETA');
 
-
+INSERT INTO Paczki (id_uzytkownika, id_skrytki, status, nadawca)
+VALUES
+    (4,3,'W_PACZKOMACIE', 'CCC'),
+    (5, NULL, 'NADANA', 'MODIVO SP. ZOO'),
+    (5, NULL, 'NADANA', 'Botland')
 
