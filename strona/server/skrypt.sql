576b17863f31be595022bb4e988c442ba18b304b
Kerowsky <kacpidos2013@gmail.com>
2025-10-11 15:41:44 +0200

    Zmiany skrypt.sql

--- strona/server/skrypt.sql
+++ strona/server/skrypt.sql
@@ -5,25 +5,27 @@ USE InteligentnaSkrytka;
 
 CREATE TABLE IF NOT EXISTS Paczki(
     id_paczki INT PRIMARY KEY AUTO_INCREMENT,
-    id_uzytkownika INT,
-    id_skrytki INT,
-    status ENUM('NADANA','W_PACZKOMACIE','ODEBRANA')
+    id_uzytkownika INT NOT NULL ,
+    id_skrytki INT NOT NULL,
+    status ENUM('NADANA','W_PACZKOMACIE','ODEBRANA') NOT NULL,
+    FOREIGN KEY (id_uzytkownika) REFERENCES Uzytkownicy(id_uzytkownika),
+    FOREIGN KEY (id_skrytki) REFERENCES Paczkomat(id_skrytki)
+
 );
 
 CREATE TABLE IF NOT EXISTS Paczkomat
 (
-    id_paczkomatu INT PRIMARY KEY AUTO_INCREMENT,
-    id_skrytki    INT,
-    rozmiar       ENUM ('S','M','L'),
+    id_skrytki    INT NOT NULL,
+    rozmiar       ENUM ('S','M','L') NOT NULL,
     status        ENUM ('WOLNA','ZAJĘTA') DEFAULT 'WOLNA'
 );
 
 CREATE TABLE IF NOT EXISTS Uzytkownicy(
-    id_uzytkownika INT,
-    haslo VARCHAR(255),
-    rola ENUM('ADMIN','KURIER','KLIENT')
+    id_uzytkownika INT  PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+    login VARCHAR(255) NOT NULL,
+    haslo VARCHAR(255) NOT NULL,
+    rola ENUM('ADMIN','KURIER','KLIENT') DEFAULT 'KLIENT'
 );
-
 
 
 
