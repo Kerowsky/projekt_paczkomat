0b73c0090e7d2a2f64b9d6c5837d0f8e9e8f1ba2
Kerowsky <kacpidos2013@gmail.com>
2025-10-12 13:38:41 +0200

    Polaczenie panelu rejestracji z baza danych, dodawanie nowo utworzonego uzytkownika

--- strona/client/index.php
+++ strona/client/index.php
@@ -10,7 +10,7 @@
 <body>
     <div>
         <h1>Log in</h1>
-        <form action="login.php" method="post">
+        <form action="login.php" method="post" style="display: ;">
             <p>
                 <input type="text" name="username" placeholder="Username" required autocomplete="off">
             </p>
@@ -24,27 +24,29 @@
                 <input type="button" value="Sign in" onclick="startRegister()">
             </p>
         </form>
-            <form action="register.php" method="post" id="registerForm" style="display: ;">
-                <p>
-                    <input type="text" name="imie" placeholder ="name" required>
-                </p>
-                <p>
-                    <input type="text" name="nazwisko" placeholder ="name" required>
-                </p>
-                <p>
-                    <input type="text" name="username" placeholder ="name" required>
-                </p>
-                <p>
-                    <input type="password" name="password" placeholder ="name" required>
-                </p>
-                <p>
-                    <input type="password" name="password" placeholder ="name" required>
-                </p>
-            </form>
+        <form action="register.php" method="post" id="registerForm" style="display:;">
+            <p>
+                <input type="button" name="close" value="X">
+            </p>
+            <p>
+                <input type="text" name="nameReg" placeholder ="Name" required>
+            </p>
+            <p>
+                <input type="text" name="surnameReg" placeholder ="Surname" required>
+            </p>
+            <p>
+                <input type="text" name="usernameReg" placeholder ="Username" required>
+            </p>
+            <p>
+                <input type="password" name="passwordReg" placeholder ="Password" required>
+            </p>
+            <p>
+                <input type="password" name="password" placeholder="Repeat password" required disabled>
+            </p>
+            <p>
+                <input type="submit" value="Sign up">
+            </p>
+        </form>
     </div>
 </body>
 </html>
-
-<?php
-echo "Gowno";
-?>
