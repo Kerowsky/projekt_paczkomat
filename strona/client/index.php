4b659f6d0ec8b882a392804f7bc88f58015a36a2
Kerowsky <kacpidos2013@gmail.com>
2025-10-12 12:38:54 +0200

    Panel rejestracji

--- strona/client/index.php
+++ strona/client/index.php
@@ -4,22 +4,47 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style/logowanie.css">
-    <title>Logowanie</title>
+    <link rel="stylesheet" href="style/register.css">
+    <title>Login</title>
 </head>
 <body>
     <div>
-        <h1>Zaloguj się</h1>
+        <h1>Log in</h1>
         <form action="login.php" method="post">
             <p>
-                <input type="text" name="username" placeholder="Nazwa użytkownika" required autocomplete="off">
+                <input type="text" name="username" placeholder="Username" required autocomplete="off">
             </p>
             <p>
-                <input type="password" name="password" placeholder="Hasło" required>
+                <input type="password" name="password" placeholder="Password" required>
             </p>
             <p>
-                <input type="submit" value="Zaloguj">
+                <input type="submit" value="Log in">
+            </p>
+            <p>
+                <input type="button" value="Sign in" onclick="startRegister()">
             </p>
         </form>
+            <form action="register.php" method="post" id="registerForm" style="display: ;">
+                <p>
+                    <input type="text" name="imie" placeholder ="name" required>
+                </p>
+                <p>
+                    <input type="text" name="nazwisko" placeholder ="name" required>
+                </p>
+                <p>
+                    <input type="text" name="username" placeholder ="name" required>
+                </p>
+                <p>
+                    <input type="password" name="password" placeholder ="name" required>
+                </p>
+                <p>
+                    <input type="password" name="password" placeholder ="name" required>
+                </p>
+            </form>
     </div>
 </body>
 </html>
+
+<?php
+echo "Gowno";
+?>
