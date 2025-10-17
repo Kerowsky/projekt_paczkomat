0a55078b8abd09b36c56af80785b515b0da2f45c
KNadska <nadskakulskikamil45@gmail.com>
2025-10-17 10:49:23 +0200

    Add files via upload

--- strona/client/delivery.php
+++ strona/client/delivery.php
@@ -20,6 +20,7 @@ if (@$conn->connect_error) {
         <link rel="stylesheet" href="style/panel.css">
         <title>NextBox</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="delivery.css">
     </head>
     <body class="bg-dark" onload="startTime()">
     <header class="p-3 mb-3 text-bg-dark">
@@ -57,15 +58,15 @@ if (@$conn->connect_error) {
     <main>
         <div class="container">
             <h1 class="text-center">LIST OF ORDERS</h1>
-                <table class="text-center table table-striped table-hover table-bordered">
-                    <thead class="table-yellowe">
+                <table class="text-center table table-striped table-hover table-bordered table-dark">
+                    <thead class="thead-warrnig text-dark">
                         <tr>
-                            <th>NUMER PACZKI</th>
-                            <th>NUMER ZAMÓWIENIA</th>
-                            <th>NADAWCA</th>
-                            <th>ODBIORCA</th>
+                            <th>PACKAGE NUMBER</th>
+                            <th>ORDER NUMBER</th>
+                            <th>SENDER</th>
+                            <th>RECIPIENT</th>
                             <th>STATUS</th>
-                            <th>AKCJA</th>
+                            <th>ACTION</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -77,7 +78,7 @@ if (@$conn->connect_error) {
                     $sql = "
                             SELECT 
                                 paczki.id_paczki,
-                                paczki.nr_zamowienia
+                                paczki.nr_zamowienia,
                                 paczki.nadawca,
                                 CONCAT(uzytkownicy.imie, ' ', uzytkownicy.nazwisko) AS odbiorca,
                                 paczki.status
@@ -88,17 +89,22 @@ if (@$conn->connect_error) {
                     $result = @$conn->query($sql);
                     if ($result->num_rows > 0) {
                         while($row = $result->fetch_assoc()) {
+                            if($row['status'] == 'W_PACZKOMACIE'){
+                                $status_text='LOCKER';
+                            }else{
+                                $status_text='ON THE WAY';
+                            }
                             echo "<tr>
-                    <td>{$row['id_paczki']}</td>
-                    <td>{$row['nr_zamowienia']}</td>
-                    <td>{$row['nadawca']}</td>
-                    <td>{$row['odbiorca']}</td>
-                    <td>{$row['status']}</td>
-                    <td>
-                        <button class='btn btn-sm btn-primary'>Otwórz paczkoamt</button>
-                        <button class='btn btn-sm btn-danger'>Zmień status</button>
-                    </td>
-                  </tr>";
+                                        <td>{$row['id_paczki']}</td>
+                                        <td>{$row['nr_zamowienia']}</td>
+                                        <td>{$row['nadawca']}</td>
+                                        <td>{$row['odbiorca']}</td>
+                                        <td>{$row['status']}</td>
+                                        <td>
+                                            <button class='btn btn-success'>Open</button>
+                                            <button class='btn btn-danger'>Change Status</button>
+                                        </td>
+                                  </tr>";
                         }
                     } else {
                         echo "<tr><td colspan='5'>Brak paczek w bazie</td></tr>";
