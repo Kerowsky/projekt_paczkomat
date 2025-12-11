e902ed9e6f19d7b88e854cdbe3b256fafc6346f5
Kerowsky <kacpidos2013@gmail.com>
2025-12-11 22:54:29 +0100

    symulacja czujnika temperatury

--- elektronika/main_arduino/main_arduino.ino
+++ elektronika/main_arduino/main_arduino.ino
@@ -3,6 +3,8 @@
 #include <WiFiS3.h>
 #include <ArduinoMDNS.h>
 
+#define simulateTempRead = 1
+
 char ssid[] = ssidWifi;
 char pass[] = passWifi;
 
@@ -49,6 +51,8 @@ int attempts = 0;
   else{
     Serial.println("Błąd mDNS");
   }
+
+  //elementy od freeRTOS
   // xTaskCreate(taskBlink, "BlinkTest",256,nullptr,2,&blinkTaskHandle);
   // xTaskCreate(tempRead, "TempRead", 256, nullptr,2,&tempReadHandle);
   xTaskCreate(taskServer, "serverHTTP", 1024, nullptr, 1, &serverHTTPHandle);
@@ -62,47 +66,40 @@ void loop() {
 
 
 void taskServer(void *pvParameters) {
-  // Opcjonalnie: upewnij się, że serwer wystartował (chyba że robisz to w setup)
-  // server.begin(); 
-
   for (;;) {
     WiFiClient client = server.available();
     mdns.run();
-    if (client) {
+if (client) {
       Serial.println("Polaczono klienta");
       String currentLine = "";
       String request = "";
-      
-      // Zabezpieczenie przed zawieszeniem (timeout)
-      unsigned long startTime = millis();
-      
-      while (client.connected()) {
-        // Jeśli minęło za dużo czasu bez danych, przerwij (np. 2 sekundy)
-        if (millis() - startTime > 2000) break; 
 
+      while (client.connected()) {
         if (client.available()) {
           char c = client.read();
           request += c;
-          
+
           if (c == '\n') {
             if (currentLine.length() == 0) {
-              // --- WYSYŁANIE ODPOWIEDZI ---
               client.println("HTTP/1.1 200 OK");
               client.println("Content-type:text/plain");
               client.println("Access-Control-Allow-Origin: *");
               client.println("Connection: close");
               client.println();
               
-              if (request.indexOf("GET /H") >= 0) {
-                digitalWrite(LED_BUILTIN, HIGH);
-                client.println("LED ON");
-                Serial.println("-> LED Wlaczona");
-              } else if (request.indexOf("GET /L") >= 0) {
-                digitalWrite(LED_BUILTIN, LOW);
-                client.println("LED OFF");
-                Serial.println("-> LED Wylaczona");
+              if (request.indexOf("GET /openLockerSmall") >= 0) {
+                Serial.println("mala szafka otworzona");
               }
-              break; 
+              else if (request.indexOf("GET /openLockerMedium") >= 0) {
+                Serial.println("srednia szafka otworzona");
+              }
+              else if (request.indexOf("GET /openLockerLarge") >= 0) {
+                Serial.println("duza szafka otworzona");
+              }
+              else if (request.indexOf("GET /tempRead") >= 0) {
+                client.write("temp");
+              }
+              break; // Wyjście z while(client.connected())
             } else {
               currentLine = "";
             }
@@ -110,20 +107,13 @@ void taskServer(void *pvParameters) {
             currentLine += c;
           }
         }
-        // Ważne w RTOS: Jeśli w pętli while czekamy na dane, dajmy odetchnąć procesorowi
         else {
              vTaskDelay(1); 
         }
       }
-      
-      // BARDZO WAŻNE: Jawne zamknięcie połączenia
       client.stop();
       Serial.println("Klient rozlaczony");
-      
-    } // koniec if(client)
-
-    // Zmienione z 500 na 10ms. 
-    // Serwer jest responsywny, ale RTOS ma czas na inne zadania.
+    } 
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
--- elektronika/main_arduino/temp.ino
+++ elektronika/main_arduino/temp.ino
@@ -1,11 +1,22 @@
 void tempRead(void *pvParameters){
+  const uint8_t simulate = simulateTempRead;
   for(;;){
-    int odczyt = analogRead(tempPin);
-    float napiecie = odczyt * (5.0/1023.0);
+    int odczyt;
+    float napiecie;
+    float temperatura;
+    switch(simulate){
+      case 0:
+          odczyt = analogRead(tempPin);
+          napiecie = odczyt * (5.0/1023.0);
 
-    float temperatura = napiecie * 100.0 - 10.0;
+          temperatura = napiecie * 100.0 - 10.0;
 
-    Serial.println(temperatura);
-    vTaskDelay(pdMS_TO_TICKS(1000));
+          Serial.println(temperatura);
+          vTaskDelay(pdMS_TO_TICKS(500));
+          break;
+        case 1: //Tryb symulowanego nadawania (do testu kolejek i strony internetowej)
+            temperatura = 20.0 + (random(0, 50) / 10.0);
+        break;
+    }
   }
 }
