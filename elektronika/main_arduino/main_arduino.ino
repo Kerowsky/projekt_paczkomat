7ed30c5c754cb76d87aa4bc1e5dce6c80ea2317b
Kerowsky <kacpidos2013@gmail.com>
2025-12-11 21:02:50 +0100

    budowa mDNS, wgrywanie na arduino

--- elektronika/main_arduino/main_arduino.ino
+++ elektronika/main_arduino/main_arduino.ino
@@ -9,14 +9,29 @@ char pass[] = passWifi;
 int port = serverPort;
 WiFiServer server(port);
 
+WiFiUDP udp;
 MDNS mdns(udp);
 const char* deviceName = "arduino";
 
 
-const TaskHandle_t blinkTaskHandle  = nullptr;
-const TaskHandle_t tempReadHandle = nullptr;
+TaskHandle_t blinkTaskHandle  = nullptr;
+TaskHandle_t tempReadHandle = nullptr;
 void setup() {
   Serial.begin(115200);
+  //Sekcja komunikacji z wifi
+  Serial.print("Lacze z ");
+  Serial.print(ssid);
+  WiFi.begin(ssid,pass);
+  Serial.println("\nPołączono IP: " + WiFi.localIP().toString());
+
+  //Start mDNS:
+  if(mdns.begin(WiFi.localIP(), deviceName)){
+    Serial.println("Uruchomiono mDNS jako: " + String(deviceName) + " .local");
+    mdns.addServiceRecord("_http", 80, MDNSServiceTCP);
+  }
+  else{
+    Serial.println("Błąd mDNS");
+  }
   xTaskCreate(taskBlink, "BlinkTest",256,nullptr,1,&blinkTaskHandle);
   xTaskCreate(tempRead, "TempRead", 256, nullptr,2,&tempReadHandle);
   vTaskStartScheduler();
