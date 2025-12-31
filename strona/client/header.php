922b462c618deed87fbe6c853b2f330462322d3a
Kerowsky <kacpidos2013@gmail.com>
2025-12-31 12:00:04 +0100

    Zachowywanie sesji po przejscu z panelu do index.php

--- strona/client/header.php
+++ strona/client/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <header class="p-3 mb-3 border-bottom text-bg-dark fixed-top">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -16,7 +19,15 @@
                 </li>
             </ul>
             <div class="text-end">
-                <a class="btn btn-warning" href="loginpanel.php">Log in</a>
+                <?php
+                if(!isset($_SESSION['zalogowany']) || $_SESSION['zalogowany'] !== true) {
+                    echo '<a class="btn btn-warning me-1" href="loginpanel.php">Log in</a>';
+                }
+                else{
+                    echo '<a class="btn btn-info me-1" href="panel.php">Panel</a>';
+                    echo '<a class="btn btn-warning me-1" href="logout.php">Log out</a>';
+                }
+                ?>
             </div>
         </div>
     </div>
