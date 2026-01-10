d9e66dd5edc16d14881910163567742ab1677734
Kerowsky <kacpidos2013@gmail.com>
2026-01-10 15:05:30 +0100

    zmiana headera

--- strona/client/header.php
+++ strona/client/header.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE){
+    session_start();
+}
 ?>
 <header class="p-3 mb-3 border-bottom text-bg-dark fixed-top">
     <div class="container">
@@ -26,6 +28,14 @@ session_start();
                 else{
                     echo '<a class="btn btn-info me-1" href="panel.php">Panel</a>';
                     echo '<a class="btn btn-warning me-1" href="logout.php">Log out</a>';
+                }
+                if(isset($_SESSION['rola'])) {
+                    if ($_SESSION['rola'] == 'KURIER' || $_SESSION['rola'] == 'ADMIN') {
+                        echo "<div class='text-end'><a class='btn btn-warning me-1' href='delivery.php'>Delivery panel</a></div>";
+                    }
+                    if ($_SESSION['rola'] == 'ADMIN') {
+                        echo "<div class='text-end'><a class='btn btn-danger me-1' href='admin.php'>Admin panel</a></div>";
+                    }
                 }
                 ?>
             </div>
